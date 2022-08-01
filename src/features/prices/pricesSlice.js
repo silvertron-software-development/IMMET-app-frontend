9b5d9770f170105ac9da3ce882b12d4f3c4d1ceb
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-08-01 14:31:05 -0500

    add service to send pricings and download pdf

--- src/features/prices/pricesSlice.js
+++ src/features/prices/pricesSlice.js
@@ -11,12 +11,22 @@ const initialState = {
 export const postNewPricing = createAsyncThunk(
   'prices/postPricing',
   async (_, thunkAPI) => {
+    const cotizaciones = thunkAPI.getState().prices.cotizaciones
+    console.log(cotizaciones)
     try {
       const { data } = await axios.post(
         'http://localhost:5000/api/v1/prices',
-        thunkAPI.getState().prices.cotizaciones
+        cotizaciones,
+        { responseType: 'blob' }
       )
-      return data
+      const file = new Blob([data], {
+        type: 'application/pdf',
+      })
+      console.log(file)
+      const fileURL = URL.createObjectURL(file)
+
+      window.open(fileURL)
+      return fileURL
     } catch (error) {
       thunkAPI.rejectWithValue(error.response.data.msg)
     }
